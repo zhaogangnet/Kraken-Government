c9bbe639946b47b62d2267a800920cdf693ffda6
ding.ding <ding.ding@china.zhaogang.com>
2016-06-28 14:58:41 +0800

    init projects

---
+++ Kraken-Government/test/Zhaogang.AspNetCore.Kraken.Government.Tests/DefaultTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zhaogang.AspNetCore.Kraken.Government.Tests
+{
+    [TestClass]
+    public class DefaultTest
+    {
+        public DefaultTest()
+        {
+        }
+
+        [TestMethod]
+        public void DefaultTester()
+        { }
+    }
+}
